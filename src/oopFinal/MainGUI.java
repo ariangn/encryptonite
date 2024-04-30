d922ba9ed7348cd6725e3d510d8f5831ffb7faea
hvandermillen <hvandermillen@gmail.com>
2024-04-29 23:48:26 -0400

    Created Basic Main Gui

--- src/oopFinal/MainGUI.java
+++ src/oopFinal/MainGUI.java
@@ -6,6 +6,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class MainGUI extends JFrame {
 
@@ -35,8 +39,27 @@ public class MainGUI extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnViewMessages = new JButton("View Messages");
+		btnViewMessages.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnViewMessages.setBounds(133, 130, 188, 29);
+		contentPane.add(btnViewMessages);
+		
+		JButton btnCreateMessage_1 = new JButton("Create Message");
+		btnCreateMessage_1.setBounds(133, 77, 188, 29);
+		contentPane.add(btnCreateMessage_1);
+		
+		JButton btnCreateCustomEncryption = new JButton("Create Custom Encryption");
+		btnCreateCustomEncryption.setBounds(133, 183, 188, 29);
+		contentPane.add(btnCreateCustomEncryption);
+		
+		JLabel lblWelcome = new JLabel("Welcome to Encrypt-o-nite!");
+		lblWelcome.setBounds(133, 25, 188, 16);
+		contentPane.add(lblWelcome);
 	}
-
 }
