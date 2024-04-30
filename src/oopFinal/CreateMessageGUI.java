dc1123978664c9238c621a8dfd2672dc88e0abda
hvandermillen <hvandermillen@gmail.com>
2024-04-30 00:22:02 -0400

    made view message gui :))))

--- src/oopFinal/CreateMessageGUI.java
+++ src/oopFinal/CreateMessageGUI.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class CreateMessageGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textMessageName;
+	private JTextField textMessage;
+	private JButton btnEncrypt;
+	private JButton btnBack;
+	private JButton btnCreate;
+	private JButton btnCreater;
 
 	/**
 	 * Launch the application.
@@ -32,11 +41,48 @@ public class CreateMessageGUI extends JFrame {
 	 */
 	public CreateMessageGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textMessageName = new JTextField();
+		textMessageName.setBounds(76, 50, 306, 26);
+		contentPane.add(textMessageName);
+		textMessageName.setColumns(10);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(22, 53, 53, 21);
+		contentPane.add(lblName);
+		
+		JLabel lblWriteTextHere = new JLabel("Write Your Message Here");
+		lblWriteTextHere.setBounds(141, 88, 162, 16);
+		contentPane.add(lblWriteTextHere);
+		
+		textMessage = new JTextField();
+		textMessage.setBounds(47, 116, 367, 177);
+		contentPane.add(textMessage);
+		textMessage.setColumns(10);
+		
+		JLabel lblCreateMessage = new JLabel("Create a Message");
+		lblCreateMessage.setBounds(165, 22, 123, 16);
+		contentPane.add(lblCreateMessage);
+		
+		btnEncrypt = new JButton("Encrypt!");
+		btnEncrypt.setBounds(171, 310, 117, 29);
+		contentPane.add(btnEncrypt);
+		
+		btnBack = new JButton("Back");
+		btnBack.setBounds(34, 366, 117, 29);
+		contentPane.add(btnBack);
+		
+		btnCreate = new JButton("Reset");
+		btnCreate.setBounds(171, 366, 117, 29);
+		contentPane.add(btnCreate);
+		
+		btnCreater = new JButton("Create");
+		btnCreater.setBounds(300, 366, 117, 29);
+		contentPane.add(btnCreater);
 	}
-
 }
