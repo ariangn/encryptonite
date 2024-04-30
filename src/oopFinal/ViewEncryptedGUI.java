56fdb3439dc1d97709d8462fc29fce13eae70d7d
hvandermillen <hvandermillen@gmail.com>
2024-04-30 02:00:29 -0400

    add a lot of gui

--- src/oopFinal/ViewEncryptedGUI.java
+++ src/oopFinal/ViewEncryptedGUI.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
 
 public class ViewEncryptedGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +37,36 @@ public class ViewEncryptedGUI extends JFrame {
 	 */
 	public ViewEncryptedGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblName = new JLabel("Name");
+		lblName.setBounds(44, 30, 54, 16);
+		contentPane.add(lblName);
+		
+		textField = new JTextField();
+		textField.setBounds(110, 25, 228, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(78, 90, 285, 118);
+		contentPane.add(textArea);
+		
+		JButton btnDeencrypt = new JButton("De-Encrypt!");
+		btnDeencrypt.setBounds(164, 220, 117, 29);
+		contentPane.add(btnDeencrypt);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(73, 284, 117, 29);
+		contentPane.add(btnBack);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setBounds(252, 284, 117, 29);
+		contentPane.add(btnDelete);
 	}
 
 }
--- src/oopFinal/ViewMessageGUI.java
+++ src/oopFinal/ViewMessageGUI.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class ViewMessageGUI extends JFrame {
 
@@ -32,11 +37,29 @@ public class ViewMessageGUI extends JFrame {
 	 */
 	public ViewMessageGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblTitle = new JLabel("View Messages");
+		lblTitle.setBounds(156, 6, 137, 16);
+		contentPane.add(lblTitle);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(429, 32, 15, 340);
+		contentPane.add(scrollBar);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(16, 375, 117, 29);
+		contentPane.add(btnBack);
+		
+		JTextArea scrollableArea = new JTextArea();
+		scrollableArea.setBounds(16, 34, 401, 329);
+		contentPane.add(scrollableArea);
+		
+		JScrollPane scroll = new JScrollPane(scrollableArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollableArea.setText("ahhahah\nhahaha\nahhahah\nhahaha\nahhahah\nhahaha\nahhahah\nhahaha\na\na\na\na\n");
 	}
-
 }
