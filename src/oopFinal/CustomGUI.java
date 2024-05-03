cd85236933a5a4582081a5dc17391614845d40bd
hvandermillen <hvandermillen@gmail.com>
2024-05-03 01:42:38 -0400

    working on minus buttons

--- src/oopFinal/CustomGUI.java
+++ src/oopFinal/CustomGUI.java
@@ -25,9 +25,15 @@ public class CustomGUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textNewFind;
-	private JPanel innerPanel;
+	private JTextField textNewReplace;
+	private JButton newMinus;
+	private JPanel outerPanel;
+	private JPanel innerPanel1;
+	private JPanel innerPanel2;
+	private JPanel innerPanel3;
 	
-	private ArrayList<JTextField> findReplaceTexts = new ArrayList<JTextField>();
+	private ArrayList<ArrayList<JTextField>> findReplaceTexts = new ArrayList<ArrayList<JTextField>>();
+	private ArrayList<JButton> minusButtons = new ArrayList<JButton>();
 
 	/**
 	 * Launch the application.
@@ -56,8 +62,17 @@ public class CustomGUI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		innerPanel = new JPanel();
-		innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
+		outerPanel = new JPanel();
+		outerPanel.setLayout(new BoxLayout(outerPanel, BoxLayout.X_AXIS));
+		innerPanel1 = new JPanel();
+		innerPanel1.setLayout(new BoxLayout(innerPanel1, BoxLayout.Y_AXIS));
+		innerPanel2 = new JPanel();
+		innerPanel2.setLayout(new BoxLayout(innerPanel2, BoxLayout.Y_AXIS));
+		innerPanel3 = new JPanel();
+		innerPanel3.setLayout(new BoxLayout(innerPanel3, BoxLayout.Y_AXIS));
+		outerPanel.add(innerPanel1);
+		outerPanel.add(innerPanel2);
+		outerPanel.add(innerPanel3);
 		
 		JLabel lblTitle = new JLabel("Create Custom Encryption");
 		lblTitle.setBounds(133, 16, 170, 16);
@@ -66,7 +81,7 @@ public class CustomGUI extends JFrame {
 		JButton btnPlus = new JButton("+");
 		btnPlus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				addFindReplaceRow(innerPanel);
+				addFindReplaceRow(innerPanel1, innerPanel2);
 			}
 		});
 		btnPlus.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
@@ -93,7 +108,7 @@ public class CustomGUI extends JFrame {
 		lblWith.setBounds(242, 69, 61, 16);
 		contentPane.add(lblWith);
 		
-		JScrollPane scrollPane = new JScrollPane(innerPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		JScrollPane scrollPane = new JScrollPane(outerPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		
 		scrollPane.setBounds(44, 91, 374, 217);
 		contentPane.add(scrollPane);
@@ -105,30 +120,78 @@ public class CustomGUI extends JFrame {
 		
 	}
 	
-	void addFindReplaceRow(JPanel innerPanel) {
+	void addFindReplaceRow(JPanel innerPanel1, JPanel innerPanel2) {
 		textNewFind = new JTextField();
 		System.out.println(findReplaceTexts.size());
 		textNewFind.setBounds(44, 10 , 100, 30 );
-		innerPanel.add(textNewFind);
+		innerPanel1.add(textNewFind);
 		textNewFind.setColumns(10);
-		innerPanel.repaint();
-		contentPane.repaint();
 		
-		findReplaceTexts.add(textNewFind);
+		textNewReplace = new JTextField();
+		textNewReplace.setBounds(144, 10 , 100, 30 );
+		innerPanel2.add(textNewReplace);
+		textNewReplace.setColumns(10);
+		
+		newMinus = new JButton("-");
+		newMinus.setBounds(244, 387, 117, 29);
+		innerPanel3.add(newMinus);
+		newMinus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				subtractRow(findReplaceTexts.size()-1);
+			}
+		});
+		
+		ArrayList<JTextField> pair = new ArrayList<JTextField>();
+		pair.add(textNewFind);
+		pair.add(textNewReplace);
+		
+		
+		findReplaceTexts.add(pair);
+		minusButtons.add(newMinus);
 		//sortFindReplace(innerPanel);
-		innerPanel.revalidate();
-		innerPanel.repaint();
-		contentPane.revalidate();
+		innerPanel1.repaint();
+		innerPanel1.revalidate();
+		innerPanel2.repaint();
+		innerPanel2.revalidate();
+		innerPanel3.repaint();
+		innerPanel3.revalidate();
+		outerPanel.repaint();
+		outerPanel.revalidate();
 		contentPane.repaint();
 	}
 	
 	void sortFindReplace(JPanel innerPanel) {
 		for (int i = 0; i < findReplaceTexts.size(); i++) {
-			findReplaceTexts.get(i).setBounds(44, 10 + (100 * i), 130, 30 + (100*i));
-			findReplaceTexts.get(i).setText(Integer.toString(i));
+			findReplaceTexts.get(i).get(0).setBounds(44, 10 + (100 * i), 130, 30 + (100*i));
+			findReplaceTexts.get(i).get(1).setBounds(44, 10 + (100 * i), 130, 30 + (100*i));
+			//findReplaceTexts.get(i).setText(Integer.toString(i));
 		}
 		
 //		innerPanel.repaint();
 //		contentPane.repaint();
+	}
+	
+	void subtractRow(int n) {
+		innerPanel1.remove(findReplaceTexts.get(n).get(0));
+		innerPanel2.remove(findReplaceTexts.get(n).get(1));
+		innerPanel3.remove(minusButtons.get(n));
+		
+		findReplaceTexts.remove(n);
+		minusButtons.remove(n);
+		
+		
+		innerPanel1.repaint();
+		innerPanel1.revalidate();
+		innerPanel2.repaint();
+		innerPanel2.revalidate();
+		innerPanel3.repaint();
+		innerPanel3.revalidate();
+		outerPanel.repaint();
+		outerPanel.revalidate();
+		contentPane.repaint();
+	}
+	
+	void clear() {
+		
 	}
 }
