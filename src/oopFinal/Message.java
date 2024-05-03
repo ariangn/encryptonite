f5bbfe605567453b078ddbf925d7914b33857677
hvandermillen <hvandermillen@gmail.com>
2024-05-03 02:56:10 -0400

    started Message.java

--- src/oopFinal/Message.java
+++ src/oopFinal/Message.java
@@ -1,5 +1,18 @@
 package oopFinal;
 
 public class Message {
+	private String messageText;
+	
+	public Message() {
+		
+	}
+	
+	public Message(String s) {
+		messageText = s;
+	}
+	
+	public String getMessage() {
+		return messageText;
+	}
 
 }
